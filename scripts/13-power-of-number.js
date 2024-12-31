6c62aba5a017100294b6a6baacc5ab6096ad214b
julesall <souleymanesall985@gmail.com>
2024-12-31 20:02:52 +0000

    convert inputs into numbers and account for negative power raise and power raise equals to 0

--- scripts/13-power-of-number.js
+++ scripts/13-power-of-number.js
@@ -1,13 +1,17 @@
 // Use recursion to find of a number
 function power(number, raise){
-    if(raise === 1){
-        return number;
+    if(raise === 0){
+        return 1;
     }
+    if (raise < 0) {
+        return 1 / power(number, -raise);
+    }
+
     return number * power(number, raise - 1);
 }
 function calculatePower(){
-    const inputNumber = document.getElementById('inputNumber').value;
-    const inputRaise = document.getElementById('inputRaise').value;
+    const inputNumber = Number(document.getElementById('inputNumber').value);
+    const inputRaise =  Number(document.getElementById('inputRaise').value);
     const result = document.getElementById('result');
     result.innerHTML = `Power of ${inputNumber} is: <b>${power(inputNumber, inputRaise)}</b>`
 }
