752ef0c1694b9e819c2bb526e3fef939f0a7f357
julesall <souleymanesall985@gmail.com>
2024-12-31 19:55:49 +0000

    change the algorithm for the bubble sorting implementation

--- scripts/12-bubble-sorting.js
+++ scripts/12-bubble-sorting.js
@@ -1,17 +1,20 @@
 // This function sorts an array using bubble sort
-function sort(){
-    const inputArray = document.getElementById('inputArray').value.split(',').map(Number);
+function sort() {
+    const inputString = document.getElementById('inputArray').value;
 
-    for(let i = 0; i < inputArray.length - 1; i++){
-       for(let j = 0; j < inputArray.length; j++){
-        if(inputArray[i] < inputArray[j]){
-            const temp = inputArray[i];
-            inputArray[i] = inputArray[j];
-            inputArray[j] = temp; 
+    // split the input string by commas and convert to numbers
+    let inputArray = inputString.split(',').map(Number);
+
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        for (let j = 0; j < inputArray.length - 1 - i; j++) {
+            if (inputArray[j] > inputArray[j + 1]) {
+                const temp = inputArray[j];
+                inputArray[j] = inputArray[j + 1];
+                inputArray[j + 1] = temp;
+            }
         }
-       }
-      
     }
+
     const result = document.getElementById('result');
     result.innerHTML = `Sorted array: <b>${inputArray}</b>`;
 }
